2cd6a7b4cd87634e467ce4b3d8df86eb48137dfd
diogro <diogro@gmail.com>
2020-02-13 18:48:58 -0300

    moving ratones stuff

--- MCMC_voles_ped.R
+++ MCMC_voles_ped.R
@@ -63,13 +63,3 @@ model = rstan::extract(animal_fit)
 colMeans(model$G)
 print(animal_fit, pars = c("G","E", "beta"))
 
-devtools::install_github("diogro/ratones")
-library(ratones)
-
-data("ratones")
-Y = sqrt(10)*as.matrix(dplyr::select(ratonesdf, IS_PM:NSL_NA))
-X = model.matrix(lm(IS_PM ~ SEX + LIN, data = ratonesdf))
-cov_fit = lmm_multiCov(Y, X)              
-model = rstan::extract(cov_fit)
-colMeans(model$P)
-cov(residuals(lm(Y~X +0)))
